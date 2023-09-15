2747a8cf29ba588a4763419e004928d5adc753fb
mirageoasis <kimhw0820@naver.com>
2023-09-16 01:25:01 +0900

    refactor : base.py typing annotaion

--- pymysqlreplication/tests/base.py
+++ pymysqlreplication/tests/base.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
 
-import pymysql
 import copy
-from pymysqlreplication import BinLogStreamReader
 import os
 import sys
+
+import pymysql
+from pymysql.connections import Connection
+from pymysql.cursors import Cursor
+
+from pymysqlreplication import BinLogStreamReader
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -15,12 +20,12 @@ base = unittest.TestCase
 
 
 class PyMySQLReplicationTestCase(base):
-    def ignoredEvents(self):
+    def ignoredEvents(self) -> list:
         return []
 
-    def setUp(self, charset="utf8"):
+    def setUp(self, charset="utf8") -> None:
         # default
-        self.database = {
+        self.database: dict = {
             "host": os.environ.get("MYSQL_5_7") or "localhost",
             "user": "root",
             "passwd": "",
@@ -30,101 +35,101 @@ class PyMySQLReplicationTestCase(base):
             "db": "pymysqlreplication_test",
         }
 
-        self.conn_control = None
-        db = copy.copy(self.database)
+        self.conn_control: None | Connection = None
+        db: dict = copy.copy(self.database)
         db["db"] = None
         self.connect_conn_control(db)
         self.execute("DROP DATABASE IF EXISTS pymysqlreplication_test")
         self.execute("CREATE DATABASE pymysqlreplication_test")
-        db = copy.copy(self.database)
+        db: dict = copy.copy(self.database)
         self.connect_conn_control(db)
-        self.stream = None
+        self.stream: BinLogStreamReader | None = None
         self.resetBinLog()
         self.isMySQL56AndMore()
         self.__is_mariaDB = None
 
-    def getMySQLVersion(self):
+    def getMySQLVersion(self) -> str:
         """Return the MySQL version of the server
         If version is 5.6.10-log the result is 5.6.10
         """
         return self.execute("SELECT VERSION()").fetchone()[0].split("-")[0]
 
-    def isMySQL56AndMore(self):
+    def isMySQL56AndMore(self) -> bool:
         version = float(self.getMySQLVersion().rsplit(".", 1)[0])
         if version >= 5.6:
             return True
         return False
 
-    def isMySQL57(self):
+    def isMySQL57(self) -> bool:
         version = float(self.getMySQLVersion().rsplit(".", 1)[0])
         return version == 5.7
 
-    def isMySQL80AndMore(self):
+    def isMySQL80AndMore(self) -> bool:
         version = float(self.getMySQLVersion().rsplit(".", 1)[0])
         return version >= 8.0
 
-    def isMySQL8014AndMore(self):
+    def isMySQL8014AndMore(self) -> bool:
         version = float(self.getMySQLVersion().rsplit(".", 1)[0])
         version_detail = int(self.getMySQLVersion().rsplit(".", 1)[1])
         if version > 8.0:
             return True
         return version == 8.0 and version_detail >= 14
 
-    def isMariaDB(self):
+    def isMariaDB(self) -> bool:
         if self.__is_mariaDB is None:
             self.__is_mariaDB = (
-                "MariaDB" in self.execute("SELECT VERSION()").fetchone()[0]
+                    "MariaDB" in self.execute("SELECT VERSION()").fetchone()[0]
             )
         return self.__is_mariaDB
 
     @property
-    def supportsGTID(self):
+    def supportsGTID(self) -> bool:
         if not self.isMySQL56AndMore():
             return False
         return self.execute("SELECT @@global.gtid_mode ").fetchone()[0] == "ON"
 
-    def connect_conn_control(self, db):
+    def connect_conn_control(self, db: dict) -> None:
         if self.conn_control is not None:
             self.conn_control.close()
         self.conn_control = pymysql.connect(**db)
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         self.conn_control.close()
         self.conn_control = None
         self.stream.close()
         self.stream = None
 
-    def execute(self, query):
-        c = self.conn_control.cursor()
+    def execute(self, query: str) -> Cursor:
+        c: Cursor = self.conn_control.cursor()
         c.execute(query)
         return c
 
-    def execute_with_args(self, query, args):
-        c = self.conn_control.cursor()
+    def execute_with_args(self, query: str, args: dict | tuple | list | None) -> Cursor:
+        c: Cursor = self.conn_control.cursor()
         c.execute(query, args)
         return c
 
-    def resetBinLog(self):
+    def resetBinLog(self) -> None:
         self.execute("RESET MASTER")
         if self.stream is not None:
             self.stream.close()
-        self.stream = BinLogStreamReader(
+        self.stream: BinLogStreamReader = BinLogStreamReader(
             self.database, server_id=1024, ignored_events=self.ignoredEvents()
         )
 
-    def set_sql_mode(self):
+    def set_sql_mode(self) -> None:
         """set sql_mode to test with same sql_mode (mysql 5.7 sql_mode default is changed)"""
         version = float(self.getMySQLVersion().rsplit(".", 1)[0])
         if version == 5.7:
             self.execute("SET @@sql_mode='NO_ENGINE_SUBSTITUTION'")
 
-    def bin_log_format(self):
+    def bin_log_format(self) -> str:
         query = "SELECT @@binlog_format"
         cursor = self.execute(query)
         result = cursor.fetchone()
         return result[0]
 
-    def bin_log_basename(self):
+    def bin_log_basename(self) -> str:
         cursor = self.execute("SELECT @@log_bin_basename")
         bin_log_basename = cursor.fetchone()[0]
         bin_log_basename = bin_log_basename.split("/")[-1]
@@ -132,9 +137,9 @@ class PyMySQLReplicationTestCase(base):
 
 
 class PyMySQLReplicationMariaDbTestCase(PyMySQLReplicationTestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         # default
-        self.database = {
+        self.database: dict = {
             "host": os.environ.get("MARIADB_10_6") or "localhost",
             "user": "root",
             "passwd": "",
@@ -145,17 +150,17 @@ class PyMySQLReplicationMariaDbTestCase(PyMySQLReplicationTestCase):
         }
 
         self.conn_control = None
-        db = copy.copy(self.database)
+        db: dict = copy.copy(self.database)
         db["db"] = None
         self.connect_conn_control(db)
         self.execute("DROP DATABASE IF EXISTS pymysqlreplication_test")
         self.execute("CREATE DATABASE pymysqlreplication_test")
-        db = copy.copy(self.database)
+        db: dict = copy.copy(self.database)
         self.connect_conn_control(db)
         self.stream = None
         self.resetBinLog()
 
-    def bin_log_basename(self):
+    def bin_log_basename(self) -> str:
         cursor = self.execute("SELECT @@log_bin_basename")
         bin_log_basename = cursor.fetchone()[0]
         bin_log_basename = bin_log_basename.split("/")[-1]
